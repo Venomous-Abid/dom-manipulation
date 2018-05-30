0bbf16831239de65c01c6ca8074aa1dff1d1d226
Abiduzzaman Abid <itisabidss@gmail.com>
2018-05-30 03:18:56 +0000

    Task 2

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 /* Task 1 */
-var heading=document.getElementById('heading')
-console.log(heading.innerText)
+var heading=document.getElementById('heading');
+console.log(heading.innerText);
 
 /* Task 2 using plain JavaScript */
 
+function addText2() {
+    var task2div = document.getElementById('task2a') ;
+    var paragraph = document.createElement('p');
+    paragraph.innerText ="Hello Ma mam";
+    task2div.appendChild(paragraph);
+}
+var textBtn =document.getElementById('textBtn2a');
+textBtn.addEventListener('click', addText2);
+
+//Task 2 b
+function changeBGcolor(event){
+    var body = document.getElementsByTagName('body')[0];
+    if(event.target.innnerText =="Red") {
+        body.style.backgroundColor ='red';
+    }
+    if(event.target.innnerText =="Green") {
+        body.style.backgroundColor ='green';
+    }
+}
 
 
+function redirectToGoogle() {
+    window.location.assign('https://www.google.com/')
+    }
 /* Task 4 using jQuery */
